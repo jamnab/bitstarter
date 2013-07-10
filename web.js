c25084911221246ad340f9faffcc7b2983f1cc4d
John Smith <example@stanford.edu>
2013-07-10 16:23:46 +0000

    Changed web.js to read out file

--- web.js
+++ web.js
@@ -1,10 +1,18 @@
 var express = require('express');
 
-var app = express.createServer(express.logger());
+ var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+var buffer = new Buffer(12);
+buffer.write("Hello World", "utf-8");
+var hello = buffer.toString("utf-8", 0, 12);
+
+var fs = require("fs");
+var data = fs.readFileSync("/bitstarter/index.html", utf-8);
+
+ app.get('/', function(request, response) {
+  response.send(hello + ' from ' + data);
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
