a254042392b2cd7acfe5c4a67c9f23638bba50b8
John Smith <example@stanford.edu>
2013-07-10 17:25:22 +0000

    Trimmed down web.js

--- web.js
+++ web.js
@@ -1,16 +1,12 @@
 var express = require('express');
-
- var app = express.createServer(express.logger());
-
-var buffer = new Buffer(12);
-buffer.write("Hello World", "utf-8");
-var hello = buffer.toString("utf-8", 0, 12);
-
 var fs = require("fs");
-var data = fs.readFileSync("/bitstarter/index.html", utf-8);
+
+var app = express.createServer(express.logger());
+
+var data = fs.readFileSync("index.html", "utf-8");
 
  app.get('/', function(request, response) {
-  response.send(hello + ' from ' + data);
+  response.send(data.toString());
 });
 
 
